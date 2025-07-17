8f454dc04289bdb7fcf249c8df47b3e1cc310461
Sameer Hussain <sameerhussain09473@gmail.com>
2025-07-18 01:36:34 +0500

    first commit

---
+++ lib/view/header/header_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeaderView extends StatelessWidget {
+  const HeaderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
